e965cf5b32b26606c81e0a0c814cf299ddf422b9
beomjun.gim3@gmail.com <beomjun.gim3@gmail.com>
2016-06-07 17:43:21 +0900

    댓글 기능 구현 및 포스트와 유저에 연결

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
@@ -10,3 +11,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 def index(request):
@@ -17,6 +18,7 @@ def post_detail(request, pk):
         })
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
@@ -28,3 +30,23 @@ def post_new(request):
     return render(request, 'blog/post_form.html', {
         'form' : form
         })
+
+
+@login_required
+def comment_new(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.user = request.user
+            comment.save()
+            return redirect('blog:post_detail', post_pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {
+        'form' : form,
+        })
+
+
