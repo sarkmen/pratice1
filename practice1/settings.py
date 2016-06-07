29f32ad4e58e7d2b9ce9ff3effe4a23c59f0caff
beomjun.gim3@gmail.com <beomjun.gim3@gmail.com>
2016-06-07 18:00:31 +0900

    로그아웃 구현 및 navbar에 로그인,로그아웃,회원가입 버튼 구현

--- practice1/settings.py
+++ practice1/settings.py
@@ -126,4 +126,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'LOGIN_URL = '/'
