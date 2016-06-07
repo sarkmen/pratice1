09e186222a2132c57e667ff87add35a5f83bbcd1
beomjun.gim3@gmail.com <beomjun.gim3@gmail.com>
2016-06-07 17:01:45 +0900

    post_form 뷰 및 템플릿 생성, url reverse 추가

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
@@ -13,4 +14,17 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {
         'post' : post
+        })
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect('blog:post_detail', post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {
+        'form' : form
         })
