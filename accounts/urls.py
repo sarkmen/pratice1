badd93e6c3f80cac8e4e62878a0435cdf8f3e828
beomjun.gim3@gmail.com <beomjun.gim3@gmail.com>
2016-06-07 17:55:22 +0900

    signup 뷰 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from django.contrib.auth.views import login
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', login),
+    url(r'^login/$', login, name='login'),
+    url(r'^signup/$', views.signup, name='signup')
 ]
