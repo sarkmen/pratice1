fff90340a91f4c42f9ec9c14ca532764d16f0a8a
beomjun.gim3@gmail.com <beomjun.gim3@gmail.com>
2016-06-07 16:24:05 +0900

    blog앱 생성 및 index 뷰 구현

--- practice1/urls.py
+++ practice1/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^$', lambda request: redirect('/blog/')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
